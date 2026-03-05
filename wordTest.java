02c9a3940bbda8f88236c486f4bbf384f7a8c606
focusstarlight <161314507+focusstarlight@users.noreply.github.com>
2026-03-05 13:40:21 +0800

    master second commit

--- wordTest.java
+++ wordTest.java
@@ -2,5 +2,6 @@ public class wordTest {
     public static void main(String[] args) {
         System.out.println("你好世界");
         System.out.println("Hello");
+        System.out.println("master-test");
     }
 }
