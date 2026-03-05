1c9d54a3b2351f5ba165482cd72b93fc40ad56aa
focusstarlight <161314507+focusstarlight@users.noreply.github.com>
2026-03-05 13:38:55 +0800

    master first commit

--- wordTest.java
+++ wordTest.java
@@ -2,5 +2,7 @@ public class wordTest {
     public static void main(String[] args) {
         System.out.println("你好世界");
         System.out.println("Hello");
+        System.out.println("hot-fix");
+        System.out.println("5678");
     }
 }
